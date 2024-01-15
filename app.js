1efdf5092c248a5414ad0d45dec65a91d4504483
Anton Latanovski <antonlatanovski@gmail.com>
2024-01-15 16:30:47 +0300

    thre

--- app.js
+++ app.js
@@ -1,12 +1,12 @@
 let but = 0;
 let up = 100;
-let found = false;
-let N = alert('загадайте число от 0 до 100')
-while(but<=up &!found){
+let found = Number(prompt('введте число от 0 до 100'));
+let N ; 
+while(but<=up & found!=N){
     N = Math.floor((up+but)/2);
     let answer = prompt(`Ваше число больше, меньше или равно ${N}? Введите "больше", "меньше" или "равно".`);
     if(answer === "равно"){ 
-    found = true;
+    found = N;
     }else if( answer==="больше"){ 
    but = N + 1;
  }else {
