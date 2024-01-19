c459a36121134ba797a51bfab0221767beb8b392
Anton Latanovski <antonlatanovski@gmail.com>
2024-01-19 16:59:13 +0300

    arrya

--- app.js
+++ app.js
@@ -1,17 +1,4 @@
-let but = 0;
-let up = 100;
-let found = Number(prompt('введте число от 0 до 100'));
-let N ; 
-while(but<=up & found!=N){
-    N = Math.floor((up+but)/2);
-    let answer = prompt(`Ваше число больше, меньше или равно ${N}? Введите "больше", "меньше" или "равно".`);
-    if(answer === "равно"){ 
-    found = N;
-    }else if( answer==="больше"){ 
-   but = N + 1;
- }else {
-    up = N-1;
- }
-}
-alert(` ваше число ${N}!`);
-    
+let arr=["понедельник","вторник","среда","четверг","пятница","суббота","воскресенье"];
+for(i=0;i<arr.length;i++){
+    alert(arr[i])
+}  
